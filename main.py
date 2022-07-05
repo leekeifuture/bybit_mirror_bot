4ab52681448adb1d069d7b551e527cf87acf5f14
Vladislav Brezovsky <opnoe09@gmail.com>
2022-07-05 19:13:25 +0300

    Add sending media and remove db

--- main.py
+++ main.py
@@ -1,24 +1,18 @@
 import logging
 
-from config import (API_HASH, API_ID, CHAT_MAPPING, DB_URL, LOG_LEVEL,
+from config import (API_HASH, API_ID, CHAT_MAPPING, LOG_LEVEL,
                     REMOVE_URLS)
 from config import REMOVE_URLS_LIST as URLS_BLACKLIST
 from config import REMOVE_URLS_WHITELIST as URLS_WHITELIST
-from config import SESSION_STRING, SOURCE_CHATS, USE_MEMORY_DB
+from config import SESSION_STRING, SOURCE_CHATS
 from telemirror.messagefilters import EmptyMessageFilter, UrlMessageFilter
 from telemirror.mirroring import MirrorTelegramClient
-from telemirror.storage import InMemoryDatabase, PostgresDatabase
 
 
 def main():
     logging.basicConfig()
     logger = logging.getLogger(__name__)
     logger.setLevel(level=LOG_LEVEL)
-
-    if USE_MEMORY_DB:
-        database = InMemoryDatabase()
-    else:
-        database = PostgresDatabase(DB_URL, logger=logger)
 
     if REMOVE_URLS:
         message_filter = UrlMessageFilter(
@@ -30,7 +24,6 @@ def main():
     client.configure_mirroring(
         source_chats=SOURCE_CHATS,
         mirror_mapping=CHAT_MAPPING,
-        database=database,
         message_filter=message_filter,
         logger=logger
     )
--- telemirror/mirroring.py
+++ telemirror/mirroring.py
@@ -1,4 +1,6 @@
 import logging
+import math
+import shutil
 from typing import Dict, List, Union
 
 import discord
@@ -9,29 +11,68 @@ from telethon.tl import types
 
 from config import DISCORD_GUILD_ID, DISCORD_BOT_TOKEN, DISCORD_CHANNEL_ID
 from .messagefilters import EmptyMessageFilter, MesssageFilter
-from .storage import Database, MirrorMessage
 
 
-async def send_to_discord_chat(text):
+async def send_to_discord_chat(message, logger):
     intents = discord.Intents.all()
-    client = NonInteractiveClient(intents=intents, text=text)
+    client = NonInteractiveClient(intents=intents, message=message,
+                                  logger=logger)
     client.run(DISCORD_BOT_TOKEN)
 
 
-async def send_message_to_discord_chat(client, text):
+async def send_message_to_discord_chat(client, message, logger):
     guild = client.get_guild(DISCORD_GUILD_ID)
     channel = guild.get_channel(DISCORD_CHANNEL_ID)
-    await channel.send(text)
+
+    if message.media:
+        downloads_dir = './downloads'
+        file_name = 'file'
+        if message.media.document and \
+                message.media.document.attributes[1].file_name:
+            file_name = message.media.document.attributes[1].file_name
+
+        logger.info('Downloading file...')
+
+        i = int(math.floor(math.log(message.file.size, 1024)))
+        p = math.pow(1024, i)
+        file_sile = round(message.file.size / p, 2)
+
+        if file_sile >= 8:
+            logger.info(
+                'Sending message without file because of more than 8MB...'
+            )
+            additional_text = f'{message.message}\n\n' \
+                if message.message \
+                else ''
+            await channel.send(
+                additional_text +
+                f'https://t.me/{message.chat.username}/{message.id}'
+            )
+        else:
+            logger.info('Downloading file...')
+            file = await message.download_media(
+                file=f'{downloads_dir}/{file_name}'
+            )
+            logger.info('Sending message with file...')
+            await channel.send(message.message, file=discord.File(file))
+
+        logger.info('Removing downloaded files...')
+        shutil.rmtree(downloads_dir)
+    else:
+        await channel.send(message.message)
+
+    logger.info('Message sent!')
 
 
 class NonInteractiveClient(discord.Client):
-    def __init__(self, *, intents=None, text=None):
+    def __init__(self, *, intents=None, message=None, logger=None):
         super().__init__(intents=intents)
-        self.text = text
+        self.message = message
+        self.logger = logger
 
     async def on_ready(self):
         await self.wait_until_ready()
-        await send_message_to_discord_chat(self, self.text)
+        await send_message_to_discord_chat(self, self.message, self.logger)
         await self.close()
 
     async def on_error(self, event, *args, **kwargs):
@@ -42,10 +83,6 @@ class EventHandlers:
     async def on_new_message(self: 'MirrorTelegramClient', event) -> None:
         """NewMessage event handler"""
 
-        if hasattr(event, 'grouped_id') and event.grouped_id is not None:
-            # skip if Album
-            return
-
         incoming_message: types.Message = event.message
         incoming_chat: int = event.chat_id
 
@@ -53,108 +90,16 @@ class EventHandlers:
             f'New message from {incoming_chat}:\n{incoming_message}')
 
         try:
-            outgoing_chats = self._mirror_mapping.get(incoming_chat)
-            if outgoing_chats is None or len(outgoing_chats) < 1:
-                self._logger.warning(f'No target chats for {incoming_chat}.')
-                return
-
-            incoming_message = self._message_filter.process(incoming_message)
-            for outgoing_chat in outgoing_chats:
-                if isinstance(incoming_message.media, types.MessageMediaPoll):
-                    await send_to_discord_chat(event.message.message)
-                    outgoing_message = await self.send_message(outgoing_chat,
-                                                               file=types.InputMediaPoll(
-                                                                   poll=incoming_message.media.poll))
-                else:
-                    await send_to_discord_chat(event.message.message)
-                    outgoing_message = await self.send_message(outgoing_chat,
-                                                               event.message)
-
-                if outgoing_message is not None:
-                    self._database.insert(
-                        MirrorMessage(original_id=incoming_message.id,
-                                      original_channel=incoming_chat,
-                                      mirror_id=outgoing_message.id,
-                                      mirror_channel=outgoing_chat))
-        except Exception as e:
-            self._logger.error(e, exc_info=True)
-
-    async def on_album(self: 'MirrorTelegramClient', event) -> None:
-        """Album event handler"""
-
-        incoming_album: List[types.Message] = event.messages
-        incoming_chat: int = event.chat_id
-
-        self._logger.info(f'New album from {incoming_chat}')
-
-        try:
-            outgoing_chats = self._mirror_mapping.get(incoming_chat)
-            if outgoing_chats is None or len(outgoing_chats) < 1:
-                self._logger.warning(f'No target chats for {incoming_chat}.')
-                return
-
-            files = []
-            captions = []
-            source_message_ids = []
-
-            for incoming_message in incoming_album:
-                incoming_message = self._message_filter.process(
-                    incoming_message)
-                files.append(incoming_message.media)
-                captions.append(incoming_message.message)
-                source_message_ids.append(incoming_message.id)
-
-            for outgoing_chat in outgoing_chats:
-                outgoing_messages = await self.send_file(
-                    outgoing_chat, caption=captions, file=files)
-
-                if outgoing_messages is not None and len(outgoing_messages) > 1:
-                    for i, outgoing_message in enumerate(outgoing_messages):
-                        self._database.insert(
-                            MirrorMessage(original_id=source_message_ids[i],
-                                          original_channel=incoming_chat,
-                                          mirror_id=outgoing_message.id,
-                                          mirror_channel=outgoing_chat))
-        except Exception as e:
-            self._logger.error(e, exc_info=True)
-
-    async def on_edit_message(self: 'MirrorTelegramClient', event) -> None:
-        """MessageEdited event handler"""
-
-        if event.message.edit_hide is True:
-            # skip if edit_hide (reactions and so on...)
-            return
-
-        incoming_message: types.Message = event.message
-        incoming_chat: int = event.chat_id
-
-        self._logger.info(
-            f'Edit message from {incoming_chat}#{incoming_message.id}')
-
-        try:
-            outgoing_messages = self._database.get_messages_to_edit(
-                incoming_message.id, incoming_chat)
-            if outgoing_messages is None or len(outgoing_messages) < 1:
-                self._logger.warning(
-                    f'No target messages for {incoming_chat}.')
-                return
-
-            incoming_message = self._message_filter.process(incoming_message)
-            for outgoing_message in outgoing_messages:
-                await self.edit_message(outgoing_message.mirror_channel,
-                                        outgoing_message.mirror_id,
-                                        incoming_message.message)
+            await send_to_discord_chat(incoming_message, self._logger)
         except Exception as e:
             self._logger.error(e, exc_info=True)
 
 
 class Mirroring(EventHandlers):
-
     def configure_mirroring(
             self: 'MirrorTelegramClient',
             source_chats: List[int],
             mirror_mapping: Dict[int, List[int]],
-            database: Database,
             message_filter: MesssageFilter = EmptyMessageFilter(),
             logger: Union[str, logging.Logger] = None
     ) -> None:
@@ -163,11 +108,9 @@ class Mirroring(EventHandlers):
         Args:
             source_chats (`List[int]`): Source chats ID list
             mirror_mapping (`Dict[int, List[int]]`): Mapping dictionary: {source: [target1, target2...]}
-            database (`Database`): Message ID storage
             message_filter (`MesssageFilter`, optional): Message filter. Defaults to `EmptyMessageFilter`.
             logger (`str` | `logging.Logger`, optional): Logger. Defaults to None.
         """
-        self._database = database
         self._mirror_mapping = mirror_mapping
         self._message_filter = message_filter
 
@@ -180,9 +123,6 @@ class Mirroring(EventHandlers):
 
         self.add_event_handler(self.on_new_message,
                                events.NewMessage(chats=source_chats))
-        self.add_event_handler(self.on_album, events.Album(chats=source_chats))
-        self.add_event_handler(self.on_edit_message,
-                               events.MessageEdited(chats=source_chats))
 
     def start_mirroring(self: 'MirrorTelegramClient') -> None:
         """Start channels mirroring"""
